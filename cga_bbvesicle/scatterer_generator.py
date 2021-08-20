a056176bf1de29bbf864ebbc07aa7cf168b778cc
zijiewu3 <zijiewu@udel.edu>
2021-08-20 16:31:21 -0400

    bbvesicle plugin functions now

--- cga_bbvesicle/scatterer_generator.py
+++ cga_bbvesicle/scatterer_generator.py
@@ -31,17 +31,17 @@ class scatterer_generator:
     bg:
         -log10(bg)
     '''
-    def __init__(self,k = 3,
+    def __init__(self,shape_params = [3],
                  minvalu = (-10,30,-200,-10,30,30,110,30,0.1),
                  maxvalu = (10,200,-30,10,200,200,700,200,4)):
-        self.k = k
+        self.k = shape_params[0]
         self._numvars = (self.k-1)*3+3
         self.minvalu = minvalu
         self.maxvalu = maxvalu
 
     @property
     def numvars(self):
-        return self._numvers
+        return self._numvars
     
     def converttoIQ(self,qrange,param):
         k = self.k
@@ -54,8 +54,8 @@ class scatterer_generator:
 
         for i in range(k-1):
             rhos[i] = param[i*3]
-            sigmas[i] = params[i*3+1]
-            epsilons[i] = params[i*3+2]
+            sigmas[i] = param[i*3+1]
+            epsilons[i] = param[i*3+2]
 
 
         bg = param[-1]
@@ -67,20 +67,26 @@ class scatterer_generator:
         epsilons[-1] = 0
 
         IQid = np.zeros((len(qrange)))
+        #print('rhos',rhos)
+        #print('sigmas',sigmas)
+        #print('epsilons',epsilons)
+        #print('r0',R0)
         for qi,q in enumerate(qrange):
             for k1 in range(k):
-                for k2 in range(k1,k):
-                    itemsp = (R0+epsilon[k1])*(R0+epsilon[k2])
-                    item1 = rho[k1] * rho[k2] * sigma[k1] * sigma[k2]
-                    item2 = np.exp(-q**2*(sigma[k1]**2+sigma[k2]**2)/2)
-                    item3 = np.cos(q*(epsilon[k1]-epsilon[k2]))
+                for k2 in range(k):
+                    itemsp = (R0 + epsilons[k1])*(R0 + epsilons[k2])
+                    item1 = rhos[k1] * rhos[k2] * sigmas[k1] * sigmas[k2]
+                    item2 = np.exp(-q**2*(sigmas[k1]**2+sigmas[k2]**2)/2)
+                    item3 = np.cos(q*(epsilons[k1]-epsilons[k2]))
                     IQid[qi] += q**(-2)*itemsp*item1*item2*item3
+        #IQid = np.sqrt(IQid)
         IQid /= IQid[0]
-        for i in IQid:
-            if i < 0:
+        for IQi in IQid:
+            if IQi < 0:
                 #if a negative value shows up, effectively throw this
                 #individual away
                 IQid = 100*np.ones(len(qrange))
+                break
         IQid += 10**(-bg)
 
         return IQid
